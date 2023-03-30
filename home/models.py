1d3b95b054ab673c17d5a2dfb273bc390ceba77d
pesnyuspoyom <kostusin31@gmail.com>
2023-03-30 10:24:43 +0300

    все готово теперь каталог

--- home/models.py
+++ home/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 
-# class Product(models.Model):
-#     name = models.CharField(max_length=100)
-#     description = models.TextField()
-#     image = models.ImageField(upload_to='products/')
-#     video = models.FileField(upload_to='videos/', null=True, blank=True)
-#     available = models.BooleanField(default=True)
-#
-#     def __str__(self):
-#         return self.name
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='products/')
+    video = models.FileField(upload_to='videos/', null=True, blank=True)
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
