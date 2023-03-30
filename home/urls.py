0e3d4d054bad3c9f68f38fd600988bd57213fc8f
pesnyuspoyom <kostusin31@gmail.com>
2023-03-30 12:32:25 +0300

    все готово

--- home/urls.py
+++ home/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('catalog/', views.catalog_view, name='catalog'),
+    path('products/<int:pk>/', views.ProductDetailView.as_view(), name='product-detail'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Product
+from django.urls import reverse
 
 def home(request):
     return render(request, 'home.html', {'title': 'Мастерим букет'})
+
+def catalog_view(request):
+    products = Product.objects.filter(available=True)
+    context = {'products': products}
+    return render(request, 'catalog.html', context)
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'product_detail.html'
