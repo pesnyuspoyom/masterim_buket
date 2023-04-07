5e1e89484da3b47a24d5e66fc077f447224cc48b
pesnyuspoyom <kostusin31@gmail.com>
2023-04-07 14:17:53 +0300

    Доработки

--- home/models.py
+++ home/models.py
@@ -4,10 +4,13 @@ from django.urls import reverse
 
 class Product(models.Model):
     name = models.CharField('Название продукта', max_length=100, unique=True)
+    announcement = models.CharField('Краткое описание продукта', max_length=100, blank=True)
     description = models.TextField('Описание продукта')
     image = models.ImageField('Фото продукта', upload_to='products/')
     video = models.FileField('Видео продукта', upload_to='videos/', null=True, blank=True)
     available = models.BooleanField('Актуален или нет', default=True)
+    price = models.IntegerField('Цена', default=1)
+    quantity = models.IntegerField('Количество', default=1)
 
     def __str__(self):
         return self.name
